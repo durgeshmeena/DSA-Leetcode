0188d175c980da156cacc7b62ccdc266546f0aba
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-05-22 20:57:00 +0530

    Time: 1316 ms (5.00%), Space: 10.8 MB (15.61%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
+    
+    int LIS(int i, int prev_i, vector<int>& nums, vector<int>&dp) {
+        if(i >= nums.size())
+            return 0;
+        
+        if(dp[prev_i+1] != -1)
+            return dp[prev_i+1];
+        
+        int the_choosen_one = 0;
+        int not_choosen = LIS(i+1, prev_i, nums, dp);
+        
+        if(prev_i == -1 || nums[i] > nums[prev_i])
+            the_choosen_one = 1 + LIS(i+1, i, nums, dp);
+        
+        return dp[prev_i+1] = max(not_choosen, the_choosen_one);
+    }
+    
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n, 1);
-        for(int i=0; i<n; i++)
-            for(int j=0; j<i; j++) {
-                if (nums[j] < nums[i] && dp[j] >= dp[i])
-                    dp[i] = dp[j]+1;
-            }
-        
-        return *max_element(dp.begin(), dp.end());
+        vector<int>dp(n+1,-1);
+        return LIS(0, -1, nums, dp);
     }
 };

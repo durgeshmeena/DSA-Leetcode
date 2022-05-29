ccd35375480662b82ab17fc3a4b0dbcdfc601c15
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-05-29 16:26:19 +0530

    Time: 18 ms (83.54%), Space: 23.8 MB (39.13%) - LeetHub

--- 563-binary-tree-tilt/563-binary-tree-tilt.cpp
+++ 563-binary-tree-tilt/563-binary-tree-tilt.cpp
@@ -12,21 +12,45 @@
 class Solution {
 public:
     
-    int sum(TreeNode* root){
-        if(!root)
-            return 0;
-        if(!root->left and !root->right){
-            return root->val;
-        }
-        else if(root->left and root->right)
-            return root->val + sum(root->left) + sum(root->right);
+//     int sum(TreeNode* root){
+//         if(!root)
+//             return 0;
+//         if(!root->left and !root->right){
+//             return root->val;
+//         }
+//         else if(root->left and root->right)
+//             return root->val + sum(root->left) + sum(root->right);
         
-        return root->val +( (root->left)?sum(root->left):sum(root->right) );
+//         return root->val +( (root->left)?sum(root->left):sum(root->right) );
+//     }
+    
+    // int findTilt(TreeNode* root) {
+    //     if(!root)
+    //         return 0;
+    //     return abs(sum(root->left) - sum(root->right)) + findTilt(root->left) + findTilt(root->right);
+    // }
+    
+    
+    int tilt=0;
+    int findTilt(TreeNode* root){
+        int d = DFS(root);
+        return tilt;
     }
     
-    int findTilt(TreeNode* root) {
+    int DFS(TreeNode* root){
         if(!root)
             return 0;
-        return abs(sum(root->left) - sum(root->right)) + findTilt(root->left) + findTilt(root->right);
+        int ls = DFS(root->left);
+        int rs = DFS(root->right);
+        tilt += abs(ls-rs);
+        return root->val + ls + rs;
     }
+    
+    
+    
+    
+    
+    
+    
+    
 };

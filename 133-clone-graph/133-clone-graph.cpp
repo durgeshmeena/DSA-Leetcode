69f78067801fdc0d79767a171c072319a33138c9
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-06-01 19:57:11 +0530

    Time: 10 ms (42.66%), Space: 8.8 MB (49.51%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -21,35 +21,55 @@ public:
 
 class Solution {
 public:
+    unordered_map<Node*, Node*>nodes;
+    
+    Node* dfs(Node* node){
+        nodes[node] = new Node(node->val);
+        for(auto it:node->neighbors){
+            if(!nodes[it])
+                nodes[node]->neighbors.push_back(dfs(it));
+            else
+                nodes[node]->neighbors.push_back(nodes[it]);
+        }
+        return nodes[node];
+        
+    }
     
     
     Node* cloneGraph(Node* node) {
-        unordered_map<int, Node*>nodes;
-        queue<Node*>q;
-        Node* nd;
-        Node* newNode = NULL;
-        if(node){
-            newNode = new Node(node->val);
-            nodes[node->val] = newNode;
-            q.push(node);
-        }
-        Node* head = newNode;
-
-        while(!q.empty()){
-            nd = q.front();
-            q.pop();
-            for(auto x : nd->neighbors){
-                if(!nodes[x->val]){
-                    Node* newNode = new Node(x->val);
-                    nodes[x->val] = newNode;
-                    q.push(x);
-                }
-                nodes[nd->val]->neighbors.push_back(nodes[x->val]);
-                    
-            }
-        }
-        
-        return head;
-        
+        if(!node)
+            return NULL;
+        return dfs(node);
     }
+    
+    
+//     Node* cloneGraph(Node* node) {
+//         unordered_map<int, Node*>nodes;
+//         queue<Node*>q;
+//         Node* nd;
+//         Node* newNode = NULL;
+//         if(node){
+//             newNode = new Node(node->val);
+//             nodes[node->val] = newNode;
+//             q.push(node);
+//         }
+//         Node* head = newNode;
+
+//         while(!q.empty()){
+//             nd = q.front();
+//             q.pop();
+//             for(auto x : nd->neighbors){
+//                 if(!nodes[x->val]){
+//                     Node* newNode = new Node(x->val);
+//                     nodes[x->val] = newNode;
+//                     q.push(x);
+//                 }
+//                 nodes[nd->val]->neighbors.push_back(nodes[x->val]);
+                    
+//             }
+//         }
+        
+//         return head;
+        
+//     }
 };

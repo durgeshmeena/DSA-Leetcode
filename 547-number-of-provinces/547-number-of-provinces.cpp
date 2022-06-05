89ffbe9d8edb81bace3567f940f341e1dc448a16
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-06-05 19:38:05 +0530

    Time: 39 ms (34.26%), Space: 13.8 MB (63.91%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,24 +1,89 @@
 class Solution {
 public:
-    
-    void dfs(int i, vector<int>&vis, vector<vector<int>>&M){
-        vis[i] = 1;
-        for(int j = 0; j < vis.size(); j++){
-            if(!vis[j] and i!=j and M[i][j])
-                dfs(j, vis, M);
-         }
+    vector<int>parent;
+    int n;
+    void unionN(int u, int v){
+        int uP = findPar(u);
+        int vP = findPar(v);
+        
+        if(uP!=vP){
+            parent[vP] = uP;
+        }
+        
     }
     
-    int findCircleNum(vector<vector<int>>&M) {
-        int n = M.size();
-        vector<int>vis(n);
+    int findPar(int u){
+        if(parent[u] == u)
+            return u;
+        return parent[u] = findPar(parent[u]);
+    }
+    
+    int findCircleNum(vector<vector<int>>& M) {
+        this->n = M.size();
+        parent.resize(n);
+        for(int i=0; i<n; i++)
+            parent[i] = i;
+
+        
+        for(int i=0; i<n; i++)
+            for(int j=i+1; j<n; j++){
+                if(M[i][j])
+                    unionN(i,j);
+            }
+        
+    
         int prov=0;
         for(int i=0; i<n; i++){
-                if(!vis[i]){
-                    dfs(i, vis, M);
-                    prov++;
-                }
+            if(parent[i]==i)
+                prov++;
         }
         return prov;
+        
+        
+        
+        
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int parent[201];
+    
+//     int findCircleNum(vector<vector<int>>& M) {
+//         int i, j, groups = 0, n = M.size();
+        
+//         for(int i = 0; i < n; i++) 
+//             parent[i] = i;
+          
+//         for(i = 0; i < n; i++) {
+//             for(j = i + 1; j < n; j++) {
+//                 if(M[i][j])
+//                     union_sets(i, j);
+//             }
+//         }
+        
+//         for(i = 0; i < n; i++) {
+//             if(i == parent[i])
+//                 groups++;
+//         }
+        
+//         return groups;
+//     }
+
+// private:
+
+//     int find_set(int v) {
+//         if (v == parent[v])
+//             return v;
+//         return parent[v] = find_set(parent[v]);
+//     }
+    
+//     void union_sets(int a, int b) {
+//         a = find_set(a);
+//         b = find_set(b);
+//         if (a != b)
+//             parent[b] = a;
+//     }
+// };

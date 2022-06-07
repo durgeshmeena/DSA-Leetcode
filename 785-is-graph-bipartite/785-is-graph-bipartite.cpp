b59325db5a766b459c38650bbf2130b181ae0139
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-06-08 00:37:00 +0530

    Time: 45 ms (20.74%), Space: 13.5 MB (79.89%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -2,33 +2,71 @@ class Solution {
 public:
     
     
-    bool dfs(int u, int color, vector<vector<int>>& adj, vector<int>& col) {
+//     bool dfs(int u, int color, vector<vector<int>>& adj, vector<int>& col) {
         
-        col[u] = color;
+//         col[u] = color;
         
-        bool f=true;
-        for(auto v : adj[u]){
-            if(col[v]==-1 and dfs(v,color^1, adj, col)==false)
-                return false;
-                // f = f and dfs(v,color^1, adj, col);
-            else
-                if(color == col[v]) return false;
+//         for(auto v : adj[u]){
+//             if(col[v]==-1){
+//                if(!dfs(v,color^1, adj, col))return false; 
+//             }
+//             else
+//                 if(color == col[v]) return false;
             
-        }
+//         }
 
+//         return true;
+        
+//     }
+    
+//     bool isBipartite(vector<vector<int>>& adj) {
+//         int n = adj.size();
+//         vector<int>col(n, -1);
+        
+//         for(int i=0; i<n; i++)
+//             if(col[i]==-1)
+//                 if(!dfs(i,0, adj, col)) return false;
+                
+        
+//         return true;
+//     }
+    
+    
+    
+    vector<int>parent;
+    
+    bool isBipartite(vector<vector<int>>& adj) {
+        int n = adj.size();
+        parent.resize(n);
+        for(int i=0;i<n;i++)
+            parent[i]=i;
+        
+        for(int i=0; i<n;i++)
+            for(int j=0; j<adj[i].size(); j++) {
+                if(find(i)==find(adj[i][j]))
+                    return false;
+                uni(adj[i][0],adj[i][j]);
+            }
+        
         return true;
         
     }
     
-    bool isBipartite(vector<vector<int>>& adj) {
-        int n = adj.size();
-        vector<int>col(n, -1);
-        
-        for(int i=0; i<n; i++)
-            if(col[i]==-1)
-                if(!dfs(i,0, adj, col)) return false;
-                
-        
-        return true;
+    
+    
+    int find(int u){
+        if(parent[u]==u)return u;
+        else{
+            return parent[u] = find(parent[u]);
+        }
     }
+    
+    void uni(int u, int v){
+        int up = find(u);
+        int vp = find(v);
+        if(up!=vp)
+            parent[vp]=up;
+    }
+    
+    
 };

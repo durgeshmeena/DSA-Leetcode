5021589c0decb05b9ac571c935af6404e264c377
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-06-13 20:12:40 +0530

    Time: 264 ms (17.69%), Space: 61.2 MB (61.36%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
     
-    bool cnd(int x, vector<int>& nums) {
-        int cnt=0;
-        for(auto num:nums){
-            if(num<=x)
-                cnt++;
-        }
-        if(cnt>x)
-            return true;
-        return false;
-    }
+    // bool cnd(int x, vector<int>& nums) {
+    //     int cnt=0;
+    //     for(auto num:nums){
+    //         if(num<=x)
+    //             cnt++;
+    //     }
+    //     if(cnt>x)
+    //         return true;
+    //     return false;
+    // }
     
     int findDuplicate(vector<int>& nums) {
         int lo=1, hi=nums.size()-1;
+        
+    
+        auto cnd = [&](int x){
+            int cnt=0;
+            for(auto num:nums){
+                if(num<=x)
+                    cnt++;
+            }
+            if(cnt>x)
+                return true;
+            return false;         
+        };
+        
         
         while(lo < hi) {
             int mid = lo + (hi-lo)/2;
             
             cout << lo << " " << hi << " -" << mid << "\n";
             
-            if(cnd(mid, nums)==false)
+            if(cnd(mid)==false)
                 lo = mid+1;
             else
                 hi=mid;

c29c60a519cbe15d0a60d26c3e3ae2e3ddf08fab
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-06-15 16:48:21 +0530

    Time: 82 ms (14.01%), Space: 89.3 MB (62.51%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,67 +1,46 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& arr1, vector<int>& arr2) {
-        int n1 = arr1.size(), n2 = arr2.size();
-        if(n1 > n2)
+        int n1 = arr1.size();
+        int n2 = arr2.size();
+        
+        if(n1>n2)
             return findMedianSortedArrays(arr2, arr1);
         
-        
-        if(n1==0){
-            if(n2%2!=0)
-                return arr2[n2/2];
-            return (arr2[-1+n2/2]+arr2[n2/2])/2.0;
-        }
-        
         int k = (n1+n2)/2;
+        int l=0, r=n1-1;
+        int arr1_l, arr1_r, arr2_l, arr2_r;
         
-        
-        int lo = 0, hi = n1-1;        
-        
-        while(lo < hi) {
-        
-            int mid1 = lo+(hi-lo)/2;
-            int mid2 = k-mid1-2;
+        while(1) {
+            int i = floor((l+r)/2.0);
+            int j = k-i-2;
             
-            if(arr1[mid1] < arr2[mid2+1])
-                lo = mid1+1;
+            
+            
+            arr1_l = i>=0?arr1[i]:-INT_MAX;
+            arr2_r = (j+1)<n2?arr2[j+1]:INT_MAX;
+            arr1_r = (i+1)<n1?arr1[i+1]:INT_MAX;
+            arr2_l = j>=0?arr2[j]:-INT_MAX;
+            
+            // cout <<"\n"<< "i:"<<i<<" j:"<<j<<"\n";
+            // cout << arr1_l << " | "<<arr1_r<<"\n";
+            // cout << arr2_l << " | "<<arr2_r<<"\n";
+            
+            if(arr1_l <= arr2_r and arr2_l <= arr1_r){
+                if((n1+n2)%2!=0)
+                    return min(arr1_r, arr2_r);
+                
+                return (max(arr1_l, arr2_l)+min(arr1_r, arr2_r))/2.0;
+            }
+            
+            else if(arr1_r < arr2_l)
+                l = i+1;
             else
-                hi = mid1;
+                r = i-1;
             
         }
         
-        int i=lo;
-        int j=k-lo-2;
-        
-        int arr1_l=arr1[i], arr1_r, arr2_l, arr2_r=arr2[j+1];
-        
-        if(arr1_l >= arr2_r){
-            i--;
-            j++;
-        }
-        
-        arr1_l = i>=0?arr1[i]:-INT_MAX;
-        arr2_r = (j+1)<n2?arr2[j+1]:INT_MAX;
-          
-        arr1_r = (i+1)<n1?arr1[i+1]:INT_MAX;
-        arr2_l = j>=0?arr2[j]:-INT_MAX;
-        
-        
-        
-  
-            
-        if((n1+n2)%2!=0)
-            return min(arr1_r, arr2_r);
-
-        return (max(arr1_l, arr2_l)+min(arr1_r, arr2_r))/2.0;
-
-        
-        
-        
-        
-        return -1.0;
-        
-        
-        
+        return -1;
         
         
     }

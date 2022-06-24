2661a4d1ff7b7f403f08aacce499afd1ed991ca1
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-06-24 16:34:32 +0530

    Time: 8 ms (87.97%), Space: 7 MB (54.03%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -4,13 +4,14 @@ public:
         int count[26] = {};
         int maxCount = 0, res = 0;
         int i=0;
+        
         for(int j=0; j<s.size(); j++){
             count[s[j]-'A']++;
             if(count[s[j]-'A']>maxCount)maxCount = count[s[j]-'A'];
             
             while(j-i+1 - maxCount > k){
                 count[s[i++]-'A']--;
-                maxCount = *max_element(count, count+26);
+                // maxCount = *max_element(count, count+26);
             }
             
             res = max(res, j-i+1);

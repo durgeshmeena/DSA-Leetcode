68110238ba9ac3adeb9b20ceace783e80bade8cd
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-06-27 17:43:15 +0530

    Time: 3 ms, Memory: 6.5 MB - LeetHub

--- pascals-triangle-ii/pascals-triangle-ii.cpp
+++ pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,15 +1,38 @@
 class Solution {
 public:
-    vector<int> getRow(int row) {
-        if(row==0)
-            return {1};
+    
+    
+    void pascal(int r, vector<int>&v){
+        if(r<0)
+            return;
         
-        vector<int>v = getRow(row-1);
+        pascal(r-1, v);
         
         for(int i=v.size()-1; i>0; i--){
             v[i] = v[i]+v[i-1];
         }
         v.push_back(1);
+        
+    }
+    
+    
+    vector<int> getRow(int row) {
+//         if(row==0)
+//             return {1};
+        
+//         vector<int>v = getRow(row-1);
+        
+//         for(int i=v.size()-1; i>0; i--){
+//             v[i] = v[i]+v[i-1];
+//         }
+//         v.push_back(1);
+        
+//         return v;
+        
+        
+        vector<int>v;
+        
+        pascal(row, v);
         
         return v;
         

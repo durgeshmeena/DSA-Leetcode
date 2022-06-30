da7791679ab4a9ec393cbf64e949be2010573903
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-06-30 19:56:57 +0530

    Time: 146 ms (6.22%), Space: 26.4 MB (5.03%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,27 +1,46 @@
 class Solution {
 public:
-    int row,col;
+    int m,n;
+    int cnt = 0;
     
-    void dfs(int i, int j, vector<vector<char>>& grid){
-        if(i<0 || j<0 || i>=row || j>=col || grid[i][j]=='0')
-            return;
-        grid[i][j]='0';
+    void BFS(int i, int j, vector<vector<char>>& B){
+        vector<int>dir = {0,-1,0,1,0};
+        queue< vector<int> > q;
+        q.push({i,j});
         
-        dfs(i,j-1,grid),dfs(i,j+1,grid);
-        dfs(i-1,j,grid),dfs(i+1,j,grid);
-        
+        while(!q.empty()){
+            int sz = q.size();
+            for(int s=0; s<sz; s++){
+                vector<int>v = q.front();
+                int i1 = v[0], j1 = v[1];
+                B[i1][j1] = '0';
+                q.pop();
+                for(int k=0; k<4; k++){
+                    int i2 = i1+dir[k], j2 = j1+dir[k+1];
+                    if(!(i2<0 || j2<0 || i2>=m || j2>=n || B[i2][j2]=='0')){
+                        B[i2][j2] = '0';
+                        q.push({i2, j2});
+                    }
+                        
+                }            
+            }
+        }
     }
     
-    int numIslands(vector<vector<char>>& grid) {
-        row=grid.size();
-        col=grid[0].size();
-        int island=0;
+    int numIslands(vector<vector<char>>& B) {
+        m = B.size();
+        n = B[0].size();
+
+        for(int i=0; i<m; i++)
+            for(int j=0; j<n; j++){
+                if(B[i][j]=='1'){
+                    cnt++;
+                    BFS(i,j, B);
+                }
+            }
         
-        for(int i=0; i<row;i++)
-            for(int j=0;j<col;j++)
-                if(grid[i][j]=='1')
-                    dfs(i,j,grid),island++;
-        return island;
+        return cnt;
+        
+        
     }
-    
 };

bffb1fba2d052e08620cea44eb2937ff80439b53
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-07-01 23:09:25 +0530

    Time: 99 ms, Memory: 12.1 MB - LeetHub

--- number-of-islands/number-of-islands.cpp
+++ number-of-islands/number-of-islands.cpp
@@ -1,46 +1,34 @@
 class Solution {
 public:
-    int m,n;
-    int cnt = 0;
     
-    void BFS(int i, int j, vector<vector<char>>& B){
-        vector<int>dir = {0,-1,0,1,0};
-        queue< vector<int> > q;
-        q.push({i,j});
+    int island = 0, m, n;
+    vector<int> dir = {-1,0,1,0,-1};
+    
+    void DFS(int i, int j, vector<vector<char>>& B){
+        if(i<0 || j<0 || i>=m || j>= n || B[i][j]=='0')
+            return;
         
-        while(!q.empty()){
-            int sz = q.size();
-            for(int s=0; s<sz; s++){
-                vector<int>v = q.front();
-                int i1 = v[0], j1 = v[1];
-                B[i1][j1] = '0';
-                q.pop();
-                for(int k=0; k<4; k++){
-                    int i2 = i1+dir[k], j2 = j1+dir[k+1];
-                    if(!(i2<0 || j2<0 || i2>=m || j2>=n || B[i2][j2]=='0'))
-                    {
-                        B[i2][j2] = '0';
-                        q.push({i2, j2});
-                    }    
-                }            
-            }
+        B[i][j]='0';
+        
+        for(int k=0; k<4; k++){
+            DFS(i+dir[k], j+dir[k+1], B);
         }
     }
     
     int numIslands(vector<vector<char>>& B) {
         m = B.size();
         n = B[0].size();
-
+        
         for(int i=0; i<m; i++)
             for(int j=0; j<n; j++){
                 if(B[i][j]=='1'){
-                    cnt++;
-                    BFS(i,j, B);
+                    island++;
+                    DFS(i,j, B);
                 }
             }
         
-        return cnt;
         
+        return island;
         
     }
 };

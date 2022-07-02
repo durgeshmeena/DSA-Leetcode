f31359a99948830f4e429dc3571d50de5b2b4860
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-07-02 12:54:13 +0530

    Time: 14 ms (34.00%), Space: 8.4 MB (88.07%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     
+    vector<int> NGE_array(vector<int>& arr){
+        int n = arr.size();
+        vector<int>res(n,-1);
+        
+        for(int i=n-2; i>=0; i--){
+            int j = i+1;
+            
+            while(j<n and arr[i] > arr[j]){
+                if(res[j]==-1)
+                    j = n;
+                else
+                    j += res[j];
+            }
+            if(j==n)res[i] = -1;
+            else
+                res[i] = j-i;
+        }
+        
+        for(int i=n-1; i>=0; i--){
+            if(res[i] != -1)
+                res[i] = arr[i+res[i]];
+        }
+        
+        return res;        
+    }
+    
     vector<int> NGR_stack(vector<int>& arr){
         int n = arr.size();
         vector<int>res(n, -1);
@@ -22,8 +48,10 @@ public:
         
     }
     
+    
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ngr2 = NGR_stack(nums2);
+        // vector<int> ngr2 = NGR_stack(nums2);
+        vector<int> ngr2 = NGE_array(nums2);
         vector<int> res;
         
         for(auto ele:ngr2)

62ca845a9b1514d064f60560c0b9fc5a7fc97aa8
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-07-16 18:16:17 +0530

    Time: 50 ms (24.20%), Space: 13.8 MB (63.37%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,89 +1,37 @@
 class Solution {
 public:
-    vector<int>parent;
-    int n;
-    void unionN(int u, int v){
-        int uP = findPar(u);
-        int vP = findPar(v);
+    int province;
+    
+    int Find(int a, vector<int>& parent){
+        int aP = parent[a];
+        if(a == aP)
+            return a;
+        return parent[a] = Find(aP, parent);
+    }
+    
+    void Union(int a, int b, vector<int>& parent){
+        int aP = Find(a, parent);
+        int bP = Find(b, parent);
         
-        if(uP!=vP){
-            parent[vP] = uP;
+        if(aP != bP){
+            parent[bP] = aP;
+            province--;
         }
-        
     }
     
-    int findPar(int u){
-        if(parent[u] == u)
-            return u;
-        return parent[u] = findPar(parent[u]);
-    }
-    
-    int findCircleNum(vector<vector<int>>& M) {
-        this->n = M.size();
-        parent.resize(n);
-        for(int i=0; i<n; i++)
-            parent[i] = i;
-
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int>parent(n+1);
+        province = n;
+        for(int i=1; i<=n; i++)
+            parent[i]=i;
         
         for(int i=0; i<n; i++)
             for(int j=i+1; j<n; j++){
-                if(M[i][j])
-                    unionN(i,j);
+                if(isConnected[i][j]) 
+                    Union(i, j, parent);
             }
         
-    
-        int prov=0;
-        for(int i=0; i<n; i++){
-            if(parent[i]==i)
-                prov++;
-        }
-        return prov;
-        
-        
-        
-        
+        return province;
     }
 };
-
-
-
-// class Solution {
-// public:
-//     int parent[201];
-    
-//     int findCircleNum(vector<vector<int>>& M) {
-//         int i, j, groups = 0, n = M.size();
-        
-//         for(int i = 0; i < n; i++) 
-//             parent[i] = i;
-          
-//         for(i = 0; i < n; i++) {
-//             for(j = i + 1; j < n; j++) {
-//                 if(M[i][j])
-//                     union_sets(i, j);
-//             }
-//         }
-        
-//         for(i = 0; i < n; i++) {
-//             if(i == parent[i])
-//                 groups++;
-//         }
-        
-//         return groups;
-//     }
-
-// private:
-
-//     int find_set(int v) {
-//         if (v == parent[v])
-//             return v;
-//         return parent[v] = find_set(parent[v]);
-//     }
-    
-//     void union_sets(int a, int b) {
-//         a = find_set(a);
-//         b = find_set(b);
-//         if (a != b)
-//             parent[b] = a;
-//     }
-// };

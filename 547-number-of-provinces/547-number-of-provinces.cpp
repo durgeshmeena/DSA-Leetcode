14cbcbb4975c21d86260fa6bc67e066392d1cbd7
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-07-16 18:39:17 +0530

    Time: 33 ms (63.05%), Space: 13.8 MB (63.37%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
-    int province;
     
-    int Find(int a, vector<int>& parent){
-        int aP = parent[a];
-        if(a == aP)
-            return a;
-        return parent[a] = Find(aP, parent);
-    }
-    
-    void Union(int a, int b, vector<int>& parent){
-        int aP = Find(a, parent);
-        int bP = Find(b, parent);
-        
-        if(aP != bP){
-            parent[bP] = aP;
-            province--;
-        }
-    }
-    
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<int>parent(n+1);
-        province = n;
-        for(int i=1; i<=n; i++)
-            parent[i]=i;
-        
-        for(int i=0; i<n; i++)
-            for(int j=i+1; j<n; j++){
-                if(isConnected[i][j]) 
-                    Union(i, j, parent);
+    void DFS(int cur, vector<int>& vis, vector<vector<int>>& M){
+        vis[cur] = 1;
+        for(int j=0; j<M.size(); j++){
+            if(M[cur][j] and !vis[j]){
+                vis[j] = 1;
+                DFS(j, vis, M);
             }
+        }
         
-        return province;
+    }
+    
+    
+    int findCircleNum(vector<vector<int>>& M) {
+        int n = M.size();
+        vector<int>vis(n);
+        int prov=0;
+        
+        for(int i=0; i<n; i++){
+            if(!vis[i]){
+                DFS(i, vis, M);
+                prov++;
+            }
+        }
+        
+        return prov;
+        
+        
     }
 };

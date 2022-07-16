dbcd9329062944780dc1268c42048bb92b379c6e
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-07-16 22:50:42 +0530

    Time: 448 ms (61.27%), Space: 122 MB (64.85%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
- 
-    int dfs(int u, vector<vector<int>>& adj, vector<int>& informTime, vector<bool>&vis){
-        vis[u]=true;
-        
-        int tChild=0;
-        for(auto it:adj[u])
-            if(!vis[it])
-               tChild = max(tChild, dfs(it,adj,informTime,vis));     
-                
-        return informTime[u]+tChild;
-        
+    
+    
+    int DFS(int cur, vector<int>& informTime, vector<vector<int>>& adj){
+        if(adj[cur].size()==0)
+            return 0;
+        int mt = 0;
+        for(auto next:adj[cur]){
+            mt = max(mt, DFS(next, informTime, adj));
+        }
+        return informTime[cur] + mt;
     }
     
+    
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        vector<vector<int>>adj(n+1);
-        vector<bool>vis(n+1);
+        vector<vector<int>> adj(n+1);
         
-        manager[headID]=n;
+        manager[headID] = n;
+        
         for(int i=0; i<n; i++){
-            // if(i!=headID)
-                adj[manager[i]].push_back(i);
+            adj[manager[i]].push_back(i);
         }
+        adj.pop_back();
+        
+        return DFS(headID, informTime, adj);
         
         
-        // for(int i=0; i<n; i++){
-        //     cout << i << " -> ";
-        //     for(auto it:adj[i])
-        //         cout << it << " ";
-        //     cout <<"\n";
-        // }
-        
-        
-        return  dfs(headID, adj, informTime, vis);
     }
 };

e024fb6e7e7bc6881632ed50deef6646cbd72030
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-07-16 23:30:04 +0530

    Time: 251 ms (94.24%), Space: 90.8 MB (96.22%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,30 +1,49 @@
 class Solution {
 public:
     
-    
-    int DFS(int cur, vector<int>& informTime, vector<vector<int>>& adj){
-        if(adj[cur].size()==0)
-            return 0;
-        int mt = 0;
-        for(auto next:adj[cur]){
-            mt = max(mt, DFS(next, informTime, adj));
-        }
-        return informTime[cur] + mt;
+    int DFS(int i, vector<int>& manager, vector<int>& informTime){
+        if(manager[i] != -1){
+            informTime[i] += DFS(manager[i], manager, informTime);
+            manager[i] = -1;
+        }    
+        return informTime[i];
+        
     }
+ 
     
     
-    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        vector<vector<int>> adj(n+1);
-        
-        manager[headID] = n;
-        
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime){
+        int res = 0;
         for(int i=0; i<n; i++){
-            adj[manager[i]].push_back(i);
+            res = max(res, DFS(i, manager, informTime));
         }
-        adj.pop_back();
-        
-        return DFS(headID, informTime, adj);
-        
-        
+        return res;
     }
+    
+    
+//     int DFS(int cur, vector<int>& informTime, vector<vector<int>>& adj){
+//         if(adj[cur].size()==0)
+//             return 0;
+//         int mt = 0;
+//         for(auto next:adj[cur]){
+//             mt = max(mt, DFS(next, informTime, adj));
+//         }
+//         return informTime[cur] + mt;
+//     }
+    
+    
+//     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+//         vector<vector<int>> adj(n+1);
+        
+//         manager[headID] = n;
+        
+//         for(int i=0; i<n; i++){
+//             adj[manager[i]].push_back(i);
+//         }
+//         adj.pop_back();
+        
+//         return DFS(headID, informTime, adj);
+        
+        
+//     }
 };

dde63b06e5995e91e92f4a1b341a9c97f5101ef4
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-07-18 10:09:50 +0530

    Time: 22 ms (97.01%), Space: 13.6 MB (59.95%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,125 +1,24 @@
 class Solution {
 public:
     
-    
-    
-    bool isBipartite(vector<vector<int>>& adj) {
-        int n = adj.size();
-        vector<int>color(n, -1);
-        
-        for(int i=0;i<n;i++){
-            if(color[i]!=-1)continue;
-            
-            queue<int>q;
-            q.push(i);
-            color[i]=0;
-            while(!q.empty()){
-                int u=q.front();
-                q.pop();
-                
-                for(auto v:adj[u]){
-                    if(color[v]==-1){
-                        color[v]= color[u]^1;
-                        q.push(v);
-                    }
-                    else{
-                        if(color[u]==color[v])
-                            return false;
-                    }
-                }
-                
-                
-            }
+    bool bipartite(int i, int val, vector<int>& color, vector<vector<int>>& adj){
+        color[i] = val;
+        for(auto next:adj[i]){
+            if(!color[next] && !bipartite(next, -val, color, adj))
+                return false;
+            else if(color[next]==val)
+                return false;
         }
         return true;
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-//     bool dfs(int u, int color, vector<vector<int>>& adj, vector<int>& col) {
-        
-//         col[u] = color;
-        
-//         for(auto v : adj[u]){
-//             if(col[v]==-1){
-//                if(!dfs(v,color^1, adj, col))return false; 
-//             }
-//             else
-//                 if(color == col[v]) return false;
-            
-//         }
-
-//         return true;
-        
-//     }
-    
-//     bool isBipartite(vector<vector<int>>& adj) {
-//         int n = adj.size();
-//         vector<int>col(n, -1);
-        
-//         for(int i=0; i<n; i++)
-//             if(col[i]==-1)
-//                 if(!dfs(i,0, adj, col)) return false;
-                
-        
-//         return true;
-//     }
-    
-    
-    
-//     vector<int>parent;
-    
-//     bool isBipartite(vector<vector<int>>& adj) {
-//         int n = adj.size();
-//         parent.resize(n);
-//         for(int i=0;i<n;i++)
-//             parent[i]=i;
-        
-//         for(int i=0; i<n;i++)
-//             for(int j=0; j<adj[i].size(); j++) {
-//                 if(find(i)==find(adj[i][j]))
-//                     return false;
-//                 uni(adj[i][0],adj[i][j]);
-//             }
-        
-//         return true;
-        
-//     }
-    
-    
-    
-//     int find(int u){
-//         if(parent[u]==u)return u;
-//         else{
-//             return parent[u] = find(parent[u]);
-//         }
-//     }
-    
-//     void uni(int u, int v){
-//         int up = find(u);
-//         int vp = find(v);
-//         if(up!=vp)
-//             parent[vp]=up;
-//     }
-    
-    
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int>color(n);
+        for(int i=0; i<n; i++){
+            if(!color[i] && !bipartite(i, 1, color, graph))
+                return false;
+        }
+        return true;
+    }
 };

637cec1cd5a9f6ab00bd43856073c6868a9d8006
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-07-18 16:50:53 +0530

    Time: 1928 ms (5.00%), Space: 271.8 MB (5.15%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,36 +1,65 @@
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<int>dist(n+1, INT_MAX);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        
-        vector<vector<pair<int, int>>> adj(n+1);
-        for(auto v:times){
-            adj[v[0]].push_back({v[1], v[2]});
-        }
-        
+    
+    int networkDelayTime(vector<vector<int>>& times, int n, int k){
+        vector<int> dist(n+1, INT_MAX-100);
         dist[k]=0;
-        pq.push({0, k});
-        while(pq.size()){
-            int prevDist = pq.top().first;
-            int prevNode = pq.top().second;
-            pq.pop();
+        vector<vector<pair<int, int>>> adj(n+1);
+        
+        
+        for(int i=0; i<n-1; i++){
             
-            for(auto next:adj[prevNode]){
-                if(dist[next.first] > next.second+dist[prevNode]){
-                    dist[next.first] = next.second+dist[prevNode];
-                    pq.push({dist[next.first], next.first});
+            for(auto edge:times){
+                // if(dist[edge[0]] == INT_MAX-100)continue;
+
+                if(dist[edge[1]] > dist[edge[0]] + edge[2]){
+                    dist[edge[1]] = dist[edge[0]] + edge[2];
                 }
+                
             }
             
-            
         }
+            
         
         int res = *max_element(dist.begin()+1, dist.end());
-        
-        return res != INT_MAX ? res : -1;
-        
-        
+        return res != INT_MAX-100 ? res : -1;
         
     }
+    
+//     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+//         vector<int>dist(n+1, INT_MAX);
+//         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        
+//         vector<vector<pair<int, int>>> adj(n+1);
+//         for(auto v:times){
+//             adj[v[0]].push_back({v[1], v[2]});
+//         }
+        
+//         dist[k]=0;
+//         pq.push({0, k});
+//         while(pq.size()){
+//             int prevDist = pq.top().first;
+//             int prevNode = pq.top().second;
+//             pq.pop();
+            
+//             for(auto next:adj[prevNode]){
+//                 if(dist[next.first] > next.second+dist[prevNode]){
+//                     dist[next.first] = next.second+dist[prevNode];
+//                     pq.push({dist[next.first], next.first});
+//                 }
+//             }
+            
+            
+//         }
+        
+//         int res = *max_element(dist.begin()+1, dist.end());
+        
+//         return res != INT_MAX ? res : -1;
+        
+        
+        
+    
+    
+//     }
+    
 };

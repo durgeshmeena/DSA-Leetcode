6cf7a7b500344314fb9dde440f05ec8794b7aff9
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-07-21 17:13:56 +0530

    Time: 242 ms (13.90%), Space: 49.4 MB (12.21%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -19,11 +19,32 @@ public:
     }
     
     int coinChange(vector<int>& coins, int amount) {
-        dp.resize(amount+1, vector<int>(coins.size()+1, -1));
-        int val = solve(amount, 0, coins);
-        if(val >= INT_MAX-2)
-            return -1;
-        return val;
+        // dp.resize(amount+1, vector<int>(coins.size()+1, -1));
+        // int val = solve(amount, 0, coins);
+        // if(val >= INT_MAX-2)
+        //     return -1;
+        // return val;
+        
+        int n = coins.size();
+        dp.resize(n+1, vector<int>(amount+1));
+        
+        
+        for(int j=1; j<=amount; j++)
+            dp[0][j]=INT_MAX-1;
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=amount; j++){
+                if(coins[i-1]<=j)
+                    dp[i][j] = min(1+dp[i][j-coins[i-1]], dp[i-1][j]);
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        
+        int val = dp[n][amount];
+        return val <= INT_MAX-2 ? val : -1;
+        
+        
         
     }
 };

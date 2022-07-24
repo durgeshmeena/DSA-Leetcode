14aa512b49768c8a1f57261c7182399d6bf62924
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-07-24 18:36:29 +0530

    Time: 18 ms (74.92%), Space: 10.5 MB (67.38%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,42 +1,19 @@
 class Solution {
 public:
-    vector<int>arr;
-    vector<int>dp;
-    
-    int LIS(int i, int prev_i){
-        if(i<0)
-            return 0;
-        
-        if(dp[prev_i+1]!=INT_MIN)
-            return dp[prev_i+1];
-        
-        if(prev_i == -1 || arr[i] < arr[prev_i])
-            return dp[prev_i+1] = max(1+LIS(i-1, i), LIS(i-1, prev_i));
-        return dp[prev_i+1] = LIS(i-1, prev_i);
-        
-    }
-    
-    
     int lengthOfLIS(vector<int>& nums) {
-        this->arr = nums;
         int n = nums.size();
-        dp.resize(n+1, INT_MIN);
-        return LIS(nums.size()-1, -1);
+        vector<int>arr({nums[0]});
         
+        for(int i=1; i<n; i++){
+            if(nums[i]>arr[arr.size()-1])
+                arr.push_back(nums[i]);
+            else{
+                auto it = lower_bound(arr.begin(), arr.end(), nums[i]);
+                *it = nums[i];
+            }
+        }
         
-        // for(int i=0; i<n; i++){
-        //     for(int j=0; j<i; j++){
-        //         if(arr[i]>arr[j])
-        //             dp[i] = max(1+dp[j], dp[i]);
-        //     }
-        // }
-        // return *max_element(dp.begin(), dp.end());
-        
-                
-            
-        // }
-        // return *max_element(dp.begin(), dp.end());
-        
+        return arr.size();
         
     }
 };

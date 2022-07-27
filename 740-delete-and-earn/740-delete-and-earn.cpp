d624828bf05b746320838d524023d70271fe6113
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-07-27 21:58:45 +0530

    Time: 50 ms (12.47%), Space: 15.2 MB (29.93%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,63 +1,24 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-
-        map<int, int> m;
-
-        for (int i=0; i<nums.size(); i++) {
-            m[nums[i]] += nums[i];
+        unordered_map<int, int> val;
+        for(auto e : nums)
+            val[e] += e;
+        set<int> s(nums.begin(), nums.end());
+        vector<int> v(s.begin(), s.end());
+        sort(v.begin(), v.end());
+        
+        int maxi = v.back();
+        
+        vector<int> dp(maxi+2, 0);
+        dp[0] = 0;
+        dp[1] = val[1];
+        
+        for(int i=2; i<=maxi; i++){
+            dp[i] = max(dp[i-1], val[i]+dp[i-2]); 
         }
-
-      
-
-        int m_s = m.size();
-        auto it = m.begin();
-
-        if (m_s == 1) return it->second;
-        if (m_s == 2) {
-            int k1, k2, v1, v2;
-            k1 = it->first;
-            v1 = it->second;
-
-            ++it;
-
-            k2 = it->first;
-            v2 = it->second;
-
-            return abs(k1-k2) != 1 ? v1 + v2 : max(v1, v2);
-
-        }
-
-
-        vector<int> dp(m_s+1);   
-
-        dp[1] = it->second;
-        int k1, k2, v1, v2;
-        k1 = it->first;
-        v1 = it->second;
-        ++it;
-        k2 = it->first;
-        v2 = it->second;
-        dp[2] = abs(k1-k2) != 1 ? v1 + v2 : max(v1, v2);
-
-        int i=3;
-
-        ++it;
-
-        for (; it != m.end(); it++) {
-
-            auto pre = prev(it, 1);
-
-            if ( abs(it->first - pre->first) == 1 ) {
-                dp[i] = max(dp[i-1], dp[i-2] + it->second);
-            } else {
-                dp[i] = dp[i-1] + it->second;
-            }
-
-
-            i++;
-        }
-        return dp[m_s];
+        
+        return dp[maxi];
+        
     }
-
 };

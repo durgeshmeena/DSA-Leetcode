cf71cba7796bf5b9491681fc7e51492fcd9ac253
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-07-28 15:28:49 +0530

    Time: 4 ms (38.68%), Space: 8.4 MB (6.53%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,37 +1,27 @@
 class Solution {
 public:
     
-    int rob2(vector<int>nums) {
-        int s = nums.size();
-        vector<int>dp(s, 0);
-        
-        dp[0] = nums[0];
-        dp[1] = max(dp[0], nums[1]);
-        
-        for (int i=2; i<s; i++) {
-            
-            dp[i] = max(dp[i-1], nums[i]+dp[i-2]);
+    int rb(vector<int>& nums){
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(2));
+        dp[0][1] = nums[0];
+        for(int i=1; i<n; i++){
+            dp[i][0] = dp[i-1][1];
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0]+nums[i]);
         }
         
-        return dp[s-1];
-        
-    }    
-    
-    int rob(vector<int>& nums){
-        int s = nums.size();
-        if (s == 1)return nums[0];
-        else if (s == 2)return max(nums[0], nums[1]);
-    
-        vector<int>v1(nums.begin(), nums.end()-1);
-        vector<int>v2(nums.begin()+1, nums.end());
-
-        
-        int f_rob = rob2(v1);    
-        int l_rob = rob2(v2);
-
-        
-        return max(f_rob, l_rob);
+        return max(dp[n-1][0], dp[n-1][1]);
     }
     
-
+    int rob(vector<int>& nums) {
+        if(nums.size()==1)
+            return nums[0];
+        else if(nums.size()==2)
+            return max(nums[0], nums[1]);
+        
+        vector<int> v(nums.begin()+1, nums.end());
+        nums.pop_back();
+        
+        return max(rb(nums), rb(v));
+    }
 };

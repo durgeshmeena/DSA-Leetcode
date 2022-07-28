afce31793333ec8284310005cf4ddcd40f2d65c5
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-07-28 15:15:07 +0530

    Time: 3 ms (54.07%), Space: 8.1 MB (5.92%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -5,8 +5,8 @@ public:
         vector<vector<int>> dp(n, vector<int>(2));
         dp[0][1] = nums[0];
         for(int i=1; i<n; i++){
-            dp[i][1] = max(dp[i-1][1], dp[i-1][0]+nums[i]);
             dp[i][0] = dp[i-1][1];
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0]+nums[i]);
         }
         
         return max(dp[n-1][0], dp[n-1][1]);

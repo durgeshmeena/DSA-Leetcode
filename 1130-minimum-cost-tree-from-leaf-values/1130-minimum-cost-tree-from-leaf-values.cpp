db32f97ae4b786cffcc0a1763368d3149f8cc6a7
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-08-05 11:00:58 +0530

    Time: 4 ms (81.20%), Space: 8.4 MB (66.91%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -20,7 +20,7 @@ public:
     int mctFromLeafValues(vector<int>& arr) {
         int n = arr.size();
     
-        memset(dp, -1, sizeof(dp));
+        // memset(dp, -1, sizeof(dp));
         memset(maxi, 0, sizeof(maxi));
         
         for(int i=0; i<n; i++){
@@ -31,7 +31,18 @@ public:
         }
             
         
-        return solve(0, n-1, arr);
+        for (int l = 1; l < n; ++l) {
+           for (int i = 0; i < n - l; ++i) {
+               int j = i + l;
+               dp[i][j] = INT_MAX;
+               for (int k = i; k < j; k++) {
+                   dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j] + maxi[i][k] * maxi[k+1][j]);
+               }
+           }
+        }
+        return dp[0][n-1];
+        
+        // return solve(0, n-1, arr);
 
     }
 };

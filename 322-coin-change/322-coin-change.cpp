030523c8f201eb103e56fa78744accd6a4592b2b
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-10-08 23:15:58 +0530

    Time: 325 ms (10.13%), Space: 49.2 MB (16.67%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,79 +1,20 @@
 class Solution {
 public:
-    
-    vector<vector<int>>dp;
-    
-    int solve(int target, int i, vector<int> &coins){
-        if(target==0)return 0;
-        else if(i==coins.size())return INT_MAX-1;
-        
-        if(dp[target][i] != -1)
-            return dp[target][i];
-        
-        int choosen = INT_MAX;
-        if(coins[i]<=target)
-            choosen = 1+solve(target-coins[i], i, coins);
-        int notChoosen = solve(target, i+1, coins);
-        return dp[target][i] =  min(choosen, notChoosen);
-        
-    }
-    
-    vector<int> memo;
-    
-    int CoinChange(int target, vector<int>& coins){
-        if(target < 0) return INT_MAX-1;
-        else if(target==0)return 0;
-        
-        if(memo[target] != -1) return memo[target];
-        
-        int res = INT_MAX-1;
-        for(int i = 0; i < coins.size(); i++){
-            res = min(res, 1 + CoinChange(target-coins[i], coins));
-        }
-        return memo[target] = res;
-        
-    }
-    
-    int coinChange(vector<int>& coins, int amount){
+    int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        memo = vector<int>(amount+1, -1);
+        vector<vector<int>> dp(n+1, vector<int>(amount+1));
+        for(int j=1; j<=amount; j++)dp[0][j] = 1e9;
         
-        int val = CoinChange(amount, coins);
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=amount; j++){
+                if(coins[i-1]<=j){
+                    dp[i][j] = min(dp[i-1][j], 1+dp[i][j-coins[i-1]]);
+                }
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
         
-        if(val < INT_MAX-2)return val;
-        return -1;
-        
+        return dp[n][amount]>=1e9?-1:dp[n][amount];
     }
-        
-        
-//     int coinChange(vector<int>& coins, int amount) {
-//         // dp.resize(amount+1, vector<int>(coins.size()+1, -1));
-//         // int val = solve(amount, 0, coins);
-//         // if(val >= INT_MAX-2)
-//         //     return -1;
-//         // return val;
-        
-//         int n = coins.size();
-//         dp.resize(n+1, vector<int>(amount+1));
-        
-        
-//         for(int j=1; j<=amount; j++)
-//             dp[0][j]=INT_MAX-1;
-        
-//         for(int i=1; i<=n; i++){
-//             for(int j=1; j<=amount; j++){
-//                 if(coins[i-1]<=j)
-//                     dp[i][j] = min(1+dp[i][j-coins[i-1]], dp[i-1][j]);
-//                 else
-//                     dp[i][j] = dp[i-1][j];
-//             }
-//         }
-        
-//         int val = dp[n][amount];
-//         return val <= INT_MAX-2 ? val : -1;
-        
-        
-        
-    
-//     }
 };

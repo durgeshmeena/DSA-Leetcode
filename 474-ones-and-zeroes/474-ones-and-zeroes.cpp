6fd183af7996ac517cd2eb3057bad73be4a13d4e
Durgesh Kumar Meena <durgeshkumar@iitg.ac.in>
2022-10-09 22:29:13 +0530

    Time: 302 ms (84.35%), Space: 102.2 MB (20.62%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
+    
+    
+    vector<vector<vector<int>>> dp;
+    
+    
     int findMaxForm(vector<string>& strs, int m, int n) {
+        int sz = strs.size();
+        dp.resize(sz, vector<vector<int>>(m+1, vector<int>(n+1, -1)));
         
-        vector<vector<int>> dp(m+1, vector<int>(n+1));
+        return solve(sz-1, m, n, strs);
         
+    }
+    
+    int solve(int i, int nzeroes, int nones, vector<string>& strs){
+        if(i<0)return 0;
+        if(nzeroes<0 || nones<0)return 0;
         
-        for(auto s : strs){
-            int zero = count(s.begin(), s.end(), '0');
-            int one = s.size()-zero;
-            
-            for(int i=m; i>=zero; i--)
-                for(int j=n; j>=one; j--){
-                    dp[i][j] = max(dp[i][j], 1+dp[i-zero][j-one]);
-                }
-            
-        }
+        if(dp[i][nzeroes][nones] != -1) return dp[i][nzeroes][nones];
         
-        return dp[m][n];
+        int zeroes = count(strs[i].begin(), strs[i].end(), '0');
+        int ones = strs[i].size()-zeroes;
+        
+        int choosen = 0;
+        if(zeroes<=nzeroes and ones<=nones)
+            choosen = 1 + solve(i-1, nzeroes-zeroes, nones-ones, strs);
+        int notchoosen = solve(i-1, nzeroes, nones, strs);
+        
+        return dp[i][nzeroes][nones] = max(choosen, notchoosen);
         
     }
 };
